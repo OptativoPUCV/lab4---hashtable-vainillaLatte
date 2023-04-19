c2ac0e20c7e0e97c7441ecd7bd4711c40cb10899
Replit user <>
2023-04-19 03:02:47 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -47,9 +47,10 @@ while(1)
     if(is_equal(map->buckets[codigo],key)==0)
     {
       map->buckets[codigo]= createPair(key,value);
+      map->size++;
       break;
     }
-    map->size++;
+    
     
   }
 }
