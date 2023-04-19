6578ac2e54253e9262a0ec1d9b60d86122908ed2
Replit user <>
2023-04-19 03:31:30 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -35,7 +35,7 @@ long hash( char * key, long capacity) {
 int is_equal(void* key1, void* key2){
     if(key1==NULL || key2==NULL) return 0;
     if(strcmp((char*)key1,(char*)key2) == 0) return 1;
-    //return 0;
+    return 0;
 }
 
 
